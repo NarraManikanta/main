374254074d8ac3d4af9eefe4cb4df4b5f3ddc7cf
Mani kanta <narrgvb123@gmail.com>
2024-02-23 04:22:24 +0530

    sql query to create a table

---
+++ stock_data_table_creation.sql
@@ -0,0 +1,11 @@
+-- create a table in oracle db 
+CREATE TABLE StockMarket_Data (
+    "Date" DATE,
+    "Open" FLOAT,
+    "High" FLOAT,
+    "Low" FLOAT,
+    "Close" FLOAT,
+    "Volume" INT,
+    "Name" VARCHAR(255),
+    "AVERAGE_STOCK_PRICE" FLOAT
+);
